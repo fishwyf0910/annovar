26da5ef9ff2d427d1f185dc473b2ce7b00100857
yufan <wangyufan22@mails.ucas.ac.cn>
2024-12-17 17:25:16 +0800

    Update annovar.bash

--- annovar.bash
+++ annovar.bash
@@ -23,13 +23,3 @@ cp genome* genome
 ##结果解读，生成2个文件
 在variant_function文件中，注释所有变异所在基因及位置。第1列为变异所在的类型，如外显子等，第2列是对应的基因名(若有多个基因名用“，”隔开)
 在exonic_variant_function文件中，详细注释外显子区域的变异功能、类型、氨基酸改变等。第1列为.variant_function文件中该变异所在行号，第2列为变异功能性后果，如外显子改变导致的氨基酸变化，阅读框移码，无义突变，终止突变等，第3列包括基因名称、转录识别标志和相应的转录本的序列变化
-## 为方便了解每一列的含义，只输出第一行，然后把制表符改成换行符
-cat anno.exonic_variant_function | head -n 1 | tr '\t' '\n'
-##查看有多少种gene variant
-cat anno.exonic_variant_function |cut -f2|cut -d" " -f1|sort |uniq -c |sort -nr
-
-  63220 synonymous
-  36036 nonsynonymous
-  15476 unknown
-    268 stopgain
-     40 stoploss
